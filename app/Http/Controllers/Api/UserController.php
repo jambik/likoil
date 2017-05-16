c3e83ae63c8805b4ebfc18173f0da293f5c1dbcd
jambik <jambik@gmail.com>
2017-05-16 16:18:54 +0300

    - обновление пароля

--- app/Http/Controllers/Api/UserController.php
+++ app/Http/Controllers/Api/UserController.php
@@ -113,7 +113,7 @@ class UserController extends ApiController
         include base_path('library/epochtasms/index.php');
 
         // Отправляем СМС
-        $res = $Stat->sendSMS("Likoil", "login: " . $card->code . "\npass: " . $password, '+7'.$card->info->phone, "", 0);
+        $res = $Stat->sendSMS("Likoil", "Likoil: " . $password, '+7'.$card->info->phone, "", 0);
 
         if (isset($res["result"]["error"])) {
             return response()->json([

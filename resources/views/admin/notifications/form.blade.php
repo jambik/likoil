cc49aefc7b714e5f031161c37afd6dc866239652
jambik <jambik@gmail.com>
2017-05-26 12:07:25 +0300

    - push-уведомления

--- resources/views/admin/notifications/form.blade.php
+++ resources/views/admin/notifications/form.blade.php
@@ -7,22 +7,22 @@
     <div class="row">
         <div class="col-lg-6 col-lg-offset-3 col-md-8 col-md-offset-2">
             <div class="row">
-                @if (isset($pushSent) && $pushSent)
+                @if (session('status'))
                     <div class="alert alert-success" role="alert">
                         <button type="button" class="close" data-dismiss="alert" aria-label="Закрыть"><span aria-hidden="true">&times;</span></button>
-                        Push сообщение отправлено
+                        {{ session('status') }}
                     </div>
                 @else
                     {!! Form::open(['url' => route('admin.notification.send'), 'method' => 'POST']) !!}
                         @if (isset($user))
                             <h3>Push сообщение пользователю - {{ $user->name }} ({{ $user->cardInfo->card->code }})</h3>
                             <p>&nbsp;</p>
-                        {!! Form::hidden('user', $user->id) !!}
+                            {!! Form::hidden('user', $user->id) !!}
                         @else
-                            <div class="form-group">
-                                {!! Form::label('device', 'Выберите устройство') !!}
-                                {!! Form::select('device', ['' => 'Все устройства', 'android' => 'Android', 'ios' => 'iOS'], null, ['class' => 'form-control']) !!}
-                            </div>
+                            {{--<div class="form-group">--}}
+                                {{--{!! Form::label('device', 'Выберите устройство') !!}--}}
+                                {{--{!! Form::select('device', ['' => 'Все устройства', 'android' => 'Android', 'ios' => 'iOS'], null, ['class' => 'form-control']) !!}--}}
+                            {{--</div>--}}
                         @endif
                         <div class="form-group">
                             {!! Form::label('message', 'Push сообщение') !!}
